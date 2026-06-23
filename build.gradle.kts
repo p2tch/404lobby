e603de2fb09922aa0bc46be4b1e78814d4fe8d94
JCode <231753395+p2tch@users.noreply.github.com>
2026-06-23 12:41:16 +0200

    chore: add required dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,18 @@ repositories {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
 
+    implementation("dev.rollczi:litecommands-bukkit:3.11.0")
+
     implementation("com.google.inject:guice:7.0.0")
+
+    implementation("com.github.xKrisSx.aaapi:core:1.0.1")
+    implementation("com.github.xKrisSx.aaapi:guice:1.0.1")
 }
 
 kotlin {
